1532b2cddc7d9a14f9f65f6cca651bb21f1d435f
Artem <zub.tema07@gmail.com>
2025-10-17 13:08:24 +0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+// ======= CONFIGURATION FILE =======
+// Настройки подключения ESP32-CAM как Wi-Fi репитера Deeper
+
+// Подключение к Deeper
+#define STA_SSID "Deeper_CHIRP"       // имя Wi-Fi диппера
+#define STA_PASS "пароль_от_диппера"  // пароль диппера
+
+// Сеть, которую создаёт ESP32
+#define AP_SSID "Deeper_Extender"     // имя новой точки Wi-Fi
+#define AP_PASS "12345678"            // пароль для подключения
+
+// ==================================
